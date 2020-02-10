6fb26dc3a66b6395fafa4317017828d0eb373d7d
Matt <36159207+Langxiupu@users.noreply.github.com>
2020-02-10 19:01:04 +0800

    Update Solution.java

--- Exercise1/Solution.java
+++ Exercise1/Solution.java
@@ -1,10 +1,10 @@
 public class Solution {
     public static int ones(int n)
     {
-        if (n == 1)
-            return 1;
+        if (n < 2)
+            return n;
 
-        return n % 2 == 0 ? ones(n/2) : 1 + ones(n / 2);
+        return n % 2 + ones(n / 2);
     }
 }
 
